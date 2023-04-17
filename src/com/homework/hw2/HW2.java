07cbee93bc86c3a4bd55ef74244b9a5fa460b119
Elena <elmikhailova86@gmail.com>
2023-04-17 23:40:07 +0300

    added tasks 1 and 2

--- src/com/homework/hw2/HW2.java
+++ src/com/homework/hw2/HW2.java
@@ -1,5 +1,37 @@
 package com.homework.hw2;
 
 public class HW2 {
+    public static void main(String[] args) {
+
+//        Задача 1
+
+        int number = 5;
+
+        if (number < 5) {
+            System.out.println("Число меньше 5");
+        } else if (number > 5) {
+            System.out.println("Число больше 5");
+        } else if (number == 5) {
+            System.out.println("Число равно 5");
+        }
+
+//        Задача 2
+
+        double height = 1.7D;
+        double weight = 50D;
+        double bodyMassIndex = weight / Math.pow(height, 2);
+        System.out.println(bodyMassIndex);
+        if (bodyMassIndex <= 16.5 ) {
+            System.out.println("Крайний недостаток веса");
+        } else if (bodyMassIndex >= 16.6) {
+            System.out.println("Недостаток в весе");
+        } else if (bodyMassIndex >= 25 ) {
+            System.out.println("Нормальный вес тела");
+        }
+
+
+
+    }
+
 
 }
