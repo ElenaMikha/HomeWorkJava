0ea29ac6937f24f1d8a16a3c2cebce211f8bd36c
Elena <elmikhailova86@gmail.com>
2023-04-17 21:21:29 +0300

    added tasks

---
+++ src/com/homework/hw1/HW1.java
@@ -0,0 +1,21 @@
+package com.homework.hw1;
+
+public class HW1 {
+    public class Main {
+        public static void main(String[] args) {
+            //        Задача 2
+
+            int x = 20;
+            x += 50;
+            System.out.println(x);
+
+            int u = 100;
+            int m = u * 2;
+            System.out.println(m);
+
+
+            double k = 1.0 / 5 * 1000;
+            System.out.println((int)k);
+        }
+    }
+}
